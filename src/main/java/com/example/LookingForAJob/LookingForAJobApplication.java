f60a0756f14706f62778a5030273f3096931573e
alexandraminyov29 <alexandra.minyov@student.upt.ro>
2022-10-19 17:48:27 +0300

    removed comment

--- src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
+++ src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LookingForAJobApplication {
-	// comment
 	public static void main(String[] args) {
 		SpringApplication.run(LookingForAJobApplication.class, args);
 	}
