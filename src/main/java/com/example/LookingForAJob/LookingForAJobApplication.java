279582a13752790285b69416b3e1fa9cfd34ab74
alexandraminyov29 <alexandraminyov773@gmail.com>
2022-10-19 19:46:52 +0300

    test

--- src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
+++ src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LookingForAJobApplication {
+public class LookingForAJobApplication { // test commit
 	public static void main(String[] args) {
 		SpringApplication.run(LookingForAJobApplication.class, args);
 	}
