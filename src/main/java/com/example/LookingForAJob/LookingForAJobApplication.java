737efa0c547bc2c864c06ae11bc2b5dd9448b554
DESKTOP-5A0R7DV\andre <andrei.hojda00@gmail.com>
2022-10-19 17:35:33 +0300

    Boilerplate code

---
+++ src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.LookingForAJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingForAJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookingForAJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LookingForAJob/LookingForAJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LookingForAJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingForAJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
