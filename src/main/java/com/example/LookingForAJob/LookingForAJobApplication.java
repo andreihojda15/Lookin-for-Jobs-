c4b7411008a8849a06f199c67dcc624afdb4a374
DESKTOP-5A0R7DV\andre <andrei.hojda00@gmail.com>
2022-10-19 17:41:59 +0300

    - test

--- src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
+++ src/main/java/com/example/LookingForAJob/LookingForAJobApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LookingForAJobApplication {
-
+	// comment
 	public static void main(String[] args) {
 		SpringApplication.run(LookingForAJobApplication.class, args);
 	}
