55f511dc5a66e5cd536cb8c7b0abbe7737a57aad
andreihojda15 <andrei.hojda00@gmail.com>
2023-01-13 16:48:44 +0200

    - apply to job endpoint - job model - disabled flyway - many to many relationship between jobs and users

--- backend/src/main/java/com/example/LookingForAJob/service/UserService.java
+++ backend/src/main/java/com/example/LookingForAJob/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.LookingForAJob.service;
 
+import com.example.LookingForAJob.model.Job;
 import com.example.LookingForAJob.model.SecurityUser;
 import com.example.LookingForAJob.model.User;
+import com.example.LookingForAJob.repository.JobRepository;
 import com.example.LookingForAJob.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -23,18 +25,25 @@ public class UserService implements UserDetailsService {
     @Autowired
     @Lazy
     PasswordEncoder encoder;
+    @Autowired
+    private JobRepository jobRepository;
 
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     public User getUser(Long id) {
-        return userRepository.findById(id).get();
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+        return user.get();
+        }else {
+            throw new EntityNotFoundException("Couldn't find user with id " + id);
+        }
     }
 
     public User login(User user) {
         Optional<User> userFound = userRepository.findByEmail(user.getEmail());
-        if(userFound.isPresent() && encoder.matches(user.getPassword(), userFound.get().getPassword())) {
+        if (userFound.isPresent() && encoder.matches(user.getPassword(), userFound.get().getPassword())) {
             return userFound.get();
         }
         throw new EntityNotFoundException("User not found");
@@ -47,20 +56,38 @@ public class UserService implements UserDetailsService {
     }
 
     public User editUser(User user, Long id) {
-        String passwordEncoded = encoder.encode(user.getPassword());
-        user.setPassword(passwordEncoded);
+
 
         return userRepository.findById(id)
                 .map(user1 -> {
-                    user1.setCv(user.getCv() != null ? user.getCv() : null);
-                    user1.setEmail(user.getEmail());
-                    user1.setImage(user.getImage() != null ? user.getImage() : null);
-                    user1.setFirstName(user.getFirstName());
-                    user1.setLastName(user.getLastName());
-                    user1.setPassword(user.getPassword());
-                    user1.setContactNumber(user.getContactNumber());
-                    user1.setDateOfBirth(user.getDateOfBirth());
-                    user1.setRole(user.getRole());
+                    if (user.getCv() != null) {
+                        user1.setCv(user.getCv());
+                    }
+                    if (user.getEmail() != null) {
+                        user1.setEmail(user.getEmail());
+                    }
+                    if (user.getImage() != null) {
+                        user1.setImage(user.getImage());
+                    }
+                    if (user.getFirstName() != null) {
+                        user1.setFirstName(user.getFirstName());
+                    }
+                    if (user.getLastName() != null) {
+                        user1.setLastName(user.getLastName());
+                    }
+                    if (user.getContactNumber() != null) {
+                        user1.setContactNumber(user.getContactNumber());
+                    }
+                    if (user.getDateOfBirth() != null) {
+                        user1.setDateOfBirth(user.getDateOfBirth());
+                    }
+                    if (user.getRole() != null) {
+                        user1.setRole(user.getRole());
+                    }
+                    if (user.getPassword() != null) {
+                        String passwordEncoded = encoder.encode(user.getPassword());
+                        user1.setPassword(passwordEncoded);
+                    }
 
                     return userRepository.save(user1);
                 })
@@ -85,4 +112,21 @@ public class UserService implements UserDetailsService {
                 .map(SecurityUser::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+
+    public void applyToJob(Long userId, Long jobId) {
+        Optional<Job> job = jobRepository.findById(jobId);
+        if(job.isEmpty()) {
+            throw new EntityNotFoundException("Couldn't find job");
+        }
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) {
+            throw new EntityNotFoundException("Couldn't find user");
+        }
+
+//        user.get().getJobs().add(job.get());
+        user.get().addJob(job.get());
+        userRepository.save(user.get());
+        jobRepository.save(job.get());
+    }
+
 }
